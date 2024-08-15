c93390b472775101bd7e7992cc3417b428eb6857
Roberto_94 <robertoverdeciasanchez@gmail.com>
2024-08-15 18:31:06 -0400

    Password checker basic example

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -7,4 +7,29 @@ describe("password checker tet suite", () => {
     sut = new PasswordChecker();
   }
   );
+
+  it("Password with less than 8 chars is invalid", () => {
+    const actual = sut.checkPassword('1234567');
+    expect(actual).toBe(false);
+  });
+  it("Password with more than 8 chars is valid", () => {
+    const actual = sut.checkPassword('12345678Aa');
+    expect(actual).toBe(true);
+  });
+  it("Password with no upper case letter is invalid", () => {
+    const actual = sut.checkPassword('1234abcd');
+    expect(actual).toBe(false);
+  });
+  it("Password with upper case letter is valid", () => {
+    const actual = sut.checkPassword('1234abcD');
+    expect(actual).toBe(true);
+  });
+  it("Password with no lower case letter is invalid", () => {
+    const actual = sut.checkPassword('1234ABCD');
+    expect(actual).toBe(false);
+  });
+  it("Password with lower case letter is valid", () => {
+    const actual = sut.checkPassword('1234abcD');
+    expect(actual).toBe(true);
+  });
 })
