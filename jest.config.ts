3900a7abe70d2fb7c1123bdaf82aa950fadd6670
Roberto_94 <robertoverdeciasanchez@gmail.com>
2024-08-17 23:13:39 -0400

    Added Server App

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,8 @@ import type { Config } from '@jest/types'
 
 /* const baseDir = '<rootDir>/src/app/pass_checker'; */
 /* const baseTestDir = '<rootDir>/src/test/pass_checker'; */
-const baseDir = '<rootDir>/src/app/doubles';
-const baseTestDir = '<rootDir>/src/test/doubles';
+const baseDir = '<rootDir>/src/app/server_app';
+const baseTestDir = '<rootDir>/src/test/server_app';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
